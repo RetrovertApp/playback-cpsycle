be9a6b7d382f22b553526d19b074b6d41d485534
Daniel Collin <daniel@collin.com>
2026-06-27 19:32:36 +0200

    viz: migrate to new viz vtable (no visualization, caps = 0)

--- cpsycle_plugin.c
+++ cpsycle_plugin.c
@@ -416,9 +416,19 @@ static RVPlaybackPlugin g_cpsycle_plugin = {
     cpsycle_plugin_metadata,
     cpsycle_plugin_static_init,
     nullptr, // settings_updated
-    nullptr, // get_tracker_info
-    nullptr, // get_pattern_cell
-    nullptr, // get_pattern_num_rows
+    nullptr, // static_destroy
+
+    // Visualization: none (caps = 0; pure decoder, no pattern grid or scope).
+    nullptr, // get_structure
+    nullptr, // get_columns
+    nullptr, // get_pattern_channels
+    nullptr, // get_scope_channels
+    nullptr, // get_position
+    nullptr, // get_channel_rows
+    nullptr, // get_cells
+    nullptr, // set_scope_enabled
+    nullptr, // get_scope_samples
+    nullptr, // get_vu
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
